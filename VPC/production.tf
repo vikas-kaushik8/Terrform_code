39dabaf7064a26b17c8f02f85f5e14ff0e541ba9
vikas-kaushik8 <98310453+vikas-kaushik8@users.noreply.github.com>
2022-01-24 15:00:46 +0530

    Create production.tf

---
+++ VPC/production.tf
@@ -0,0 +1,8 @@
+source = "./modules/networking"
+  region               = "${var.region}"
+  environment          = "${var.environment}"
+  vpc_cidr             = "${var.vpc_cidr}"
+  public_subnets_cidr  = "${var.public_subnets_cidr}"
+  private_subnets_cidr = "${var.private_subnets_cidr}"
+  availability_zones   = "${local.production_availability_zones}"
+}
